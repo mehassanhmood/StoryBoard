35a0f41e688cbf6a5d7db7a3f4e5d7c9dbd29e2b
Hassan Mehmood <mehassanhmood@gmail.com>
2024-11-19 01:57:49 -0500

    repo name fixed

--- src/components/DynamicStory.jsx
+++ src/components/DynamicStory.jsx
@@ -6,18 +6,18 @@ const DynamicStory = () => {
       <div className="flex flex-col border rounded-3xl border-teal-600 justify-between items-center w-[96] h-[600px] p-4 m-4 gap-4">
             <div className='flex flex-row justify-between items-center h-1/2 w-full'>
                 <div className="sub-boxes justify-center items-center text-center m-2 bg-teal-700 h-full w-1/2">
-                    Box 1
+                    Line Chart
                 </div>
                 <div className="sub-boxes justify-center items-center text-center  m-2 bg-teal-700 h-full w-1/2">
-                    Box 2
+                    Bar Chart
                 </div>
             </div>
             <div className='flex flex-row justify-center items-center h-1/2 w-full'>
                 <div className="sub-boxes justify-center items-center text-center  m-2 bg-teal-700 h-full w-1/2">
-                    Box 3
+                    Donut Chart
                 </div>
                 <div className="sub-boxes justify-center items-center text-center  m-2 bg-teal-700 h-full w-1/2">
-                    Box 4
+                    Some other Chart
                 </div>
             </div>
         </div>
