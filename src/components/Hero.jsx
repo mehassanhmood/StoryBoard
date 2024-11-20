5c0af5975757ee688d5a9db8e1dfd4b2a60dc4a0
Hassan Mehmood <mehassanhmood@gmail.com>
2024-11-20 04:43:27 -0500

    responsiveness fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,23 +4,24 @@ import { delay, motion } from "framer-motion"
 
 const Hero = () => {
 
-  const question = "Compelling Question or a stat to engage the user"
+  const question = "Compelling Question or a statistics to engage the user"
+  const letters = question.split(" ")
   const letterAnimation = {
-    hidden: {opacity:0},
+    hidden: {opacity:0, y:20},
     visible: (i) => ({
       opacity: 1,
       y:0,
       transition: {
-        delay: i * 0.05,
+        delay: i * 0.1,
         duration: 2,
-        ease:"easeIn"
+        ease:"easeInOut"
       }
     })
   }
 
 
   return (
-    <div className='flex flex-col h-[100vh] w-[100vw]'>
+    <div className='flex flex-col h-[100vh] w-[100vw] overflow-hidden'>
 
       <div className='flex flex-col leading-8 jutify-center items-center relative p-6 m-6'>
         <h1 className='text-6xl text-center tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-purple-500'>The Ripple Effect: How Economic Trends Shape Canadian Lives</h1>
@@ -29,17 +30,18 @@ const Hero = () => {
 
       <div className='flex h-full m-2 p-6 justify-center items-center relative gap-6'>
         <motion.div className="w-[35%]">
-          <p className="flex flex-wrap text-4xl text-pretty text-transparent bg-clip-text bg-gradient-to-r from-teal-100 to-cyan-300">
-            {question.split("").map((letter, index) => (
+          <p className="flex flex-wrap md:text-4xl text-pretty text-transparent bg-clip-text bg-gradient-to-r from-teal-100 to-cyan-300">
+            {letters.map((letter, index) => (
                 <motion.span
+                className='mr-2'
                 key={index}
                 custom={index}
                 initial={"hidden"}
                 animate={"visible"}
                 variants={letterAnimation}
                 >
-                  {letter === " " ? "\u00A0": letter}
-                  
+                  {/* {letter === " " ? "\u00A0": letter} */}
+                  {letter}
                    
                 </motion.span>
             ))
@@ -53,7 +55,7 @@ const Hero = () => {
         transition={{duration:2, ease:"easeInOut"}}
         >
           <img src={placeholderImage} alt="Hero image of inflation vs investment" 
-          className='h-full object-cover border border-teal-600 rounded-3xl opacity-80'
+          className='h-[250px] md:object-fit  border border-teal-600 rounded-3xl opacity-80'
           />
         </motion.div>
       </div>
