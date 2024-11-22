f7bf26413858aeeea3430313a9fcca373630ad67
Hassan Mehmood <mehassanhmood@gmail.com>
2024-11-22 04:18:21 -0500

    inflation data added

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -44,10 +44,11 @@ const LineChart = () => {
     return `M${points.join(" L")}`; // Join points into a valid SVG path
   }, [price]);
 
-  const year_2000 = useTransform(scrollYProgress, [0.1,0.2], [0, 1]);
-  const dot_com = useTransform(scrollYProgress, [0.15,0.2], [0, 1]);
-  const housing_crisis = useTransform(scrollYProgress, [0.3,0.4], [0, 1]);
-  const covid_strikes = useTransform(scrollYProgress, [0.72,0.8], [0, 1]);
+  const year_2000 = useTransform(scrollYProgress, [0.1,0.2], [0, 0.8]);
+  const dot_com = useTransform(scrollYProgress, [0.15,0.2], [0, 0.8]);
+  const housing_crisis = useTransform(scrollYProgress, [0.3,0.4], [0, 0.8]);
+  const covid_strikes = useTransform(scrollYProgress, [0.72,0.8], [0, 0.8]);
+  const inf_high = useTransform(scrollYProgress, [0.8,0.85], [0, 0.8]);
 
   
 
@@ -78,31 +79,47 @@ const LineChart = () => {
 
         <motion.div
           style={{ opacity: year_2000 }} // Opacity controlled by scroll progress
-          className="absolute top-[55%] md:top-[40%] ml-16 md:left[4%] translate-x-[-50%] translate-y-[-50%] text-white text-sm md:text-xl font-bold"
+          className="absolute top-[55%] md:top-[45%] ml-10 md:left[4%] translate-x-[-50%] translate-y-[-50%] text-white text-sm md:text-md"
         >
           Year 2000
         </motion.div>
 
         <motion.div
           style={{ opacity: dot_com }} // Opacity controlled by scroll progress
-          className="absolute top-[65%] left-[15%]  md:left-[10%] translate-x-[-50%] translate-y-[-50%] text-white text-sm md:text-xl font-bold"
+          className="absolute top-[62%] left-[15%]  md:left-[12%] translate-x-[-50%] translate-y-[-50%] text-white text-sm md:text-md"
         >
           Dot com bubble
         </motion.div>
 
         <motion.div
           style={{ opacity: housing_crisis }} // Opacity controlled by scroll progress
-          className="absolute top-[60%] left-[40%] translate-x-[-50%] translate-y-[-50%] text-white text-sm md:text-xl font-bold"
+          className="absolute top-[60%] left-[40%] translate-x-[-50%] translate-y-[-50%] text-white text-sm md:text-md"
         >
           The Housing Crisis
+        </motion.div>
+        <motion.div
+          style={{ opacity: housing_crisis }} // Opacity controlled by scroll progress
+          className="absolute top-[67%] md:top-[67%] left-[40%] translate-x-[-50%] translate-y-[-50%] text-white text-xs md:text-md text-green-400 w-[20px]"
+        >
+          Lowest Infaltion in 24 years 0.3%
         </motion.div>
 
         <motion.div
           style={{ opacity: covid_strikes }} // Opacity controlled by scroll progress
-          className="absolute top-[60%] md:top-[42%] left-[85%] translate-x-[-50%] translate-y-[-50%] text-white text-sm md:text-xl font-bold"
+          className="absolute top-[60%] md:top-[45%] left-[85%] translate-x-[-50%] translate-y-[-50%] text-white text-sm md:text-md "
         >
           Covid Strikes
         </motion.div>
+
+        <motion.div
+          style={{ opacity: inf_high }} // Opacity controlled by scroll progress
+          className="absolute top-[45%] md:top-[18%] left-[95%] translate-x-[-50%] translate-y-[-50%] text-white text-sm md:text-md text-red-400"
+        >
+          24 Year High Infation 6.8%
+        </motion.div>
+
+     
+
 
       </motion.div>
     </div>
