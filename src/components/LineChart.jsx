a8f726e9eb9b43c8be9cfc0d0f79fcdb131aa339
Hassan Mehmood <mehassanhmood@gmail.com>
2024-11-22 02:21:53 -0500

    fadein fadeout

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -37,19 +37,27 @@ const LineChart = () => {
 
     const points = price.map((value, index) => {
       const x = index * scaleX;
-      const y = 560 - value * scaleY; // Flip and scale Y-axis
+      const y = 600 - value * scaleY; // Flip and scale Y-axis
       return `${x},${y}`;
     });
 
     return `M${points.join(" L")}`; // Join points into a valid SVG path
   }, [price]);
 
-  
+  const year_2000 = useTransform(scrollYProgress, [0.1,0.2], [0, 1]);
+  const dot_com = useTransform(scrollYProgress, [0.15,0.2], [0, 1]);
+  const housing_crisis = useTransform(scrollYProgress, [0.3,0.4], [0, 1]);
+  const covid_strikes = useTransform(scrollYProgress, [0.72,0.8], [0, 1]);
+
   
 
   return (
-    <div className="relative h-[900vh] w-full bg-cyan-950/50" ref={containerRef}>
+    <div className="relative h-[950vh] w-full bg-cyan-950/50" ref={containerRef}>
+
       <motion.div className="sticky top-0 h-[100vh]">
+      <h1
+      className="text-2xl  flex justify-center mt-6 pt-6"
+      > Lets relive he ups and downs of the candian economic indicator: TSX</h1>
         <motion.svg
           initial="hidden"
           animate="visible"
@@ -67,6 +75,35 @@ const LineChart = () => {
             style={{ pathLength: useTransform(scrollYProgress, [0, 1], [0, 1]) }}
           />
         </motion.svg>
+
+        <motion.div
+          style={{ opacity: year_2000 }} // Opacity controlled by scroll progress
+          className="absolute top-[40%] left-[3%] translate-x-[-50%] translate-y-[-50%] text-white text-xl font-bold"
+        >
+          Year 2000
+        </motion.div>
+
+        <motion.div
+          style={{ opacity: dot_com }} // Opacity controlled by scroll progress
+          className="absolute top-[65%] left-[10%] translate-x-[-50%] translate-y-[-50%] text-white text-xl font-bold"
+        >
+          The dot com bubble
+        </motion.div>
+
+        <motion.div
+          style={{ opacity: housing_crisis }} // Opacity controlled by scroll progress
+          className="absolute top-[60%] left-[40%] translate-x-[-50%] translate-y-[-50%] text-white text-xl font-bold"
+        >
+          The Housing Crisis
+        </motion.div>
+
+        <motion.div
+          style={{ opacity: covid_strikes }} // Opacity controlled by scroll progress
+          className="absolute top-[42%] left-[85%] translate-x-[-50%] translate-y-[-50%] text-white text-xl font-bold"
+        >
+          Covid Strikes
+        </motion.div>
+
       </motion.div>
     </div>
   );
