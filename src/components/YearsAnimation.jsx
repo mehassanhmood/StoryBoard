f1c4842b9bd3f19b57e8fa23a33688bebaa084aa
Hassan Mehmood <mehassanhmood@gmail.com>
2024-11-26 16:57:10 -0500

    mobile responsiveness fixed

--- src/components/YearsAnimation.jsx
+++ src/components/YearsAnimation.jsx
@@ -13,19 +13,19 @@ const YearsAnimation = ({refernece}) => {
   const inflationRef = useRef(null);
   const bondRef = useRef(null);
 
-  const housingInView = useInView(housingRef, {once: false, amount:1})
+  const housingInView = useInView(housingRef, {once: false, amount:0.7})
   const InflationInView = useInView(inflationRef, {once: false})
-  const bondInView = useInView(bondRef, {once: false, amount:1})
+  const bondInView = useInView(bondRef, {once: false, amount:0.7})
 
 
 
   return (
     
-    <div ref={refernece} className='flex flex-col h-[120vh] justify-between md:flex-row gap-6 h-screen bg-gradient-to-r from-cyan-500/50 to-purple-500/50 '>
-      <div className='flex h-full w-[50%]'>
+    <div ref={refernece} className='flex flex-col h-[120vh] justify-between md:flex-row gap-2 h-screen bg-gradient-to-r from-cyan-500/50 to-purple-500/50 '>
+      <div className='flex h-full w-[full] md:w-[50%] sticky top-0 mt-5'>
         <DynamicStory housingInView={housingInView} inflationInView={InflationInView} bondInView={bondInView}/>
       </div>
-      <div className='w-[50%] h-full h-[70%]'>
+      <div className='md:w-[50%] md:h-full h-[80%]'>
         <Narrative text={<LoremIpsum housingRef={ housingRef } inflationRef={inflationRef} bondRef={bondRef}/>} />
        </div>
     </div>
