f8c62b9ee4056533240e5d1e1839a1f1a1ba5170
Hassan Mehmood <mehassanhmood@gmail.com>
2024-11-28 02:12:30 -0500

    more icon refinement

--- src/assets/LoremIpsum.jsx
+++ src/assets/LoremIpsum.jsx
@@ -7,7 +7,7 @@ export function LoremIpsum({housingRef, inflationRef, bondRef}) {
     return (
       <>
         <div className="content-section">
-        <img src={inflation_pic} className ="mb-10 md:ml-10 md pl-10" alt="" />
+        <img src={inflation_pic} className ="mb-10 md:ml-10 md pl-10 opacity-10" alt="" />
     
           <h2 
             className="text-center text-2xl mb-[50px] md:mt-[100px]">The Effect of Inflation on the Dollar (2000–2024)</h2>
@@ -16,7 +16,7 @@ export function LoremIpsum({housingRef, inflationRef, bondRef}) {
               Over the last two decades, inflation has significantly eroded the purchasing power of the dollar. In 2000, one dollar could buy much more than it can today, with inflation steadily increasing at an average annual rate of 2-3%. By 2024, the cumulative impact of inflation has reduced the real value of a dollar by nearly 40%, meaning that what cost $1 in 2000 might now cost approximately $1.40 or more. This silent erosion highlights the importance of understanding inflation and its impact on savings and investments.
             </p>
 
-          <img src={housing_market} className ="mb-10 md:ml-10 md pl-10" alt="" />
+          <img src={housing_market} className ="mb-10 md:ml-10 md pl-10 opacity-20" alt="" />
           <h2 className="text-center text-2xl pt-12 mb-10">
             Growth of a Dollar If Invested in Housing
           </h2>
@@ -25,7 +25,7 @@ export function LoremIpsum({housingRef, inflationRef, bondRef}) {
           ref={housingRef}>
             Housing has proven to be one of the most resilient investments over the years. From 2000 to 2024, the real estate market experienced substantial growth, driven by demand, urbanization, and limited housing supply in many regions. A dollar invested in housing in 2000 would have grown significantly, benefiting from property appreciation and rental income. For example, in major cities, property values have doubled or tripled, turning even modest investments into substantial wealth. This underscores the potential of real estate as a long-term wealth-building asset.
           </p>
-          <img src={index_fund} className ="mb-10 md:ml-10 md pl-10" alt="" />
+          <img src={index_fund} className ="mb-10 md:ml-10 md pl-10 opacity-20" alt="" />
           <h2 
             className="text-center text-2xl">Why People Are Afraid to Invest in an Index Fund</h2>
           <br />
