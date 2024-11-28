dbda604534f71758fcd95b497361bcb124d70320
Hassan Mehmood <mehassanhmood@gmail.com>
2024-11-28 17:25:28 -0500

    Footer Done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex justify-center items-center bg-gradient-to-r from-cyan-500 to-purple-500  p-4'>
-      <p className='text-black font-bold'>
-        Footer Section
+    <div className='flex flex-col justify-center bg-gradient-to-r from-cyan-500 to-purple-500  p-4 h-[30vh]'>
+      <p className='text-white '>
+        <h1 className='font-bold text-2xl '>References</h1>
+        <ul className='list-disc ml-4'>
+          <li>Yahoo Finance</li>
+          <li>Statistics Canada</li>
+          <li>Statista</li>
+        </ul>
       </p>
     </div>
   )
 }
 
 export default Footer
+
+
